1423a439efd56c85b15d5eb9228a6b60ab46eccb
dietmarja <dietmarja@gmail.com>
2024-10-07 16:52:18 +0100

    Delete persona2interviews/output_generators.tmp directory

--- persona2interviews/output_generators.tmp/base_generator.py
+++
@@ -1,8 +0,0 @@
-# File: output_generators/base_generator.py
-
-from abc import ABC, abstractmethod
-
-class BaseOutputGenerator(ABC):
-    @abstractmethod
-    def generate(self, input_data, persona):
-        pass
